ca7cf4b08b83a237fb23ebb91ae897096704a89c
dogagelisli <dogagelisli@gmail.com>
2019-01-15 03:26:24 +0300

    Update

--- MailGönderme.py
+++ MailGönderme.py
@@ -1,18 +1,37 @@
 import smtplib
-print("####################")
-print("        d4nt        ")
-print("####################")
+import getpass
+import sys
+print("""
+███████╗    ███╗   ███╗ █████╗ ██╗██╗         ██████╗  ██████╗ ███╗   ███╗██████╗ ███████╗██████╗
+██╔════╝    ████╗ ████║██╔══██╗██║██║         ██╔══██╗██╔═══██╗████╗ ████║██╔══██╗██╔════╝██╔══██╗
+█████╗█████╗██╔████╔██║███████║██║██║         ██████╔╝██║   ██║██╔████╔██║██████╔╝█████╗  ██████╔╝
+██╔══╝╚════╝██║╚██╔╝██║██╔══██║██║██║         ██╔══██╗██║   ██║██║╚██╔╝██║██╔══██╗██╔══╝  ██╔══██╗
+███████╗    ██║ ╚═╝ ██║██║  ██║██║███████╗    ██████╔╝╚██████╔╝██║ ╚═╝ ██║██████╔╝███████╗██║  ██║
+╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
 
-content = input("Mesajınızı giriniz: ")
-mail = smtplib.SMTP("smtp.gmail.com",587)
-mail.ehlo()
-mail.starttls()
-login = input("Gönderici Mail adresinizi giriniz: ")
-pw = input("Gönderici Mail adresinizin parolasını giriniz: ")
-mail.login(login,pw)
-mail1=input("Göndermek istediğiniz mail adresini yazınız: ")
-sayi = int(input("Kaçtane mail göndermek istiyorsunuz ?: "))
-for a in range(sayi):
-    mail.sendmail(login,mail1,content)
-    print("+\n", end="")
-print("BAŞARIYLA GÖNDERDİNİZ")
+███╗   ███╗ █████╗ ██████╗ ███████╗    ██████╗ ██╗   ██╗    ██████╗ ██╗  ██╗███╗   ██╗████████╗
+████╗ ████║██╔══██╗██╔══██╗██╔════╝    ██╔══██╗╚██╗ ██╔╝    ██╔══██╗██║  ██║████╗  ██║╚══██╔══╝
+██╔████╔██║███████║██║  ██║█████╗      ██████╔╝ ╚████╔╝     ██║  ██║███████║██╔██╗ ██║   ██║
+██║╚██╔╝██║██╔══██║██║  ██║██╔══╝      ██╔══██╗  ╚██╔╝      ██║  ██║╚════██║██║╚██╗██║   ██║
+██║ ╚═╝ ██║██║  ██║██████╔╝███████╗    ██████╔╝   ██║       ██████╔╝     ██║██║ ╚████║   ██║
+╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝    ╚═════╝    ╚═╝       ╚═════╝      ╚═╝╚═╝  ╚═══╝   ╚═╝
+
+""")
+try:
+    content = input("Message:")
+    mail = smtplib.SMTP("smtp.gmail.com",587)
+    mail.ehlo()
+    mail.starttls()
+    login = input("Sender mail: ")
+    pw = getpass.getpass("Sender mail password: ")
+    mail.login(login,pw)
+    mail1=input("Receiver mail adress: ")
+    sayi = int(input("How many mail do you want to send ?: "))
+
+    for a in range(sayi):
+        mail.sendmail(login,mail1,content)
+        print("+", end="")
+        print(sayi,"Succesful!")
+except smtplib.SMTPAuthenticationError:
+    print("Username or password wrong!")
+    sys.exit()
